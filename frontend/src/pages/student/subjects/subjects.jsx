30c8380e80d8f3e893a9b360e1de5f4e6fbb9f70
Erlindi Isaj <erlind.isaj02@gmail.com>
2024-05-31 19:25:52 +0300

    loader added

--- frontend/src/pages/student/subjects/subjects.jsx
+++ frontend/src/pages/student/subjects/subjects.jsx
@@ -2,21 +2,24 @@ import axios from "axios";
 
 import { useEffect, useState } from "react";
 
-import { Box, Button } from "@mui/material";
+import { Box, Button, CircularProgress } from "@mui/material";
 
 import { useSelector } from "react-redux";
 import { selectProgram } from "store/program/program.selector";
 
 import { getToken } from "utils/helper-functions";
 import SubjectsTable from "./SubjectsTable";
+import { selectUserToken } from "store/user/user.selector";
 
 const Subjects = () => {
   const program = useSelector(selectProgram);
+  const token = useSelector(selectUserToken);
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [subjects, setSubjects] = useState([]);
 
   useEffect(() => {
-    const token = getToken();
+    setLoading(true);
     axios
       .get(
         "http://localhost:5158/api/University/Faculty/Departments/Student/Courses/Details",
@@ -29,6 +32,7 @@ const Subjects = () => {
       )
       .then((response) => {
         setSubjects(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -48,31 +52,47 @@ const Subjects = () => {
         justifyContent: "center",
       }}
     >
-      {Object.keys(result).map((year, index) => {
-        const subjectsForYear = result[year];
-        if (index === 0 || open) {
-          return <SubjectsTable subjects={subjectsForYear} />;
-        }
-      })}
-      <Box sx={{ margin: "50px" }}>
-        <Button
-          onClick={() => setOpen(!open)}
-          type="submit"
-          size="large"
-          variant="contained"
-          style={{
-            height: "50px",
-            borderRadius: "12px",
-            marginTop: "60px",
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "200px",
           }}
-          fullWidth={true}
         >
-          {" "}
-          {open
-            ? "Geçmiş Dönem Derslerini Gizle"
-            : "Geçmiş Dönem Derslerini Göster"}
-        </Button>
-      </Box>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          {Object.keys(result).map((year, index) => {
+            const subjectsForYear = result[year];
+            if (index === 0 || open) {
+              return <SubjectsTable subjects={subjectsForYear} />;
+            }
+          })}
+
+          <Box sx={{ margin: "50px" }}>
+            <Button
+              onClick={() => setOpen(!open)}
+              type="submit"
+              size="large"
+              variant="contained"
+              style={{
+                height: "50px",
+                borderRadius: "12px",
+                marginTop: "60px",
+              }}
+              fullWidth={true}
+            >
+              {" "}
+              {open
+                ? "Geçmiş Dönem Derslerini Gizle"
+                : "Geçmiş Dönem Derslerini Göster"}
+            </Button>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };

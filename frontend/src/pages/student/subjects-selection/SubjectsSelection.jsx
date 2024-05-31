9073e974a6b2d2c56f961c867a0c02fff8ba7111
Erlindi Isaj <erlind.isaj02@gmail.com>
2024-05-31 19:15:11 +0300

    student conntected to api

--- frontend/src/pages/student/subjects-selection/SubjectsSelection.jsx
+++ frontend/src/pages/student/subjects-selection/SubjectsSelection.jsx
@@ -1,19 +1,66 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 import { Box } from "@mui/material";
 
 import InfoHeader from "./components/InfoHeader";
 import SecDersler from "./components/Secdersler";
 import SeciliDersler from "./components/SeciliDersler";
+import { useSelector } from "react-redux";
+import { selectUserToken } from "store/user/user.selector";
+import { selectProgram } from "store/program/program.selector";
 
 const SubjectsSelection = () => {
+  const token = useSelector(selectUserToken);
+  const department = useSelector(selectProgram);
+
+  const [data, setData] = useState([]);
+  const [details, setDetails] = useState();
+
+  useEffect(() => {
+    try {
+      axios
+        .get(
+          "http://localhost:5158/api/University/Faculty/Department/Semester/Student/Courses",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            params: { DepName: department },
+          }
+        )
+        .then((response) => {
+          setData(response.data);
+        });
+      axios
+        .get(
+          "http://localhost:5158/api/University/Faculty/Departments/Student/Details",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          setDetails(response.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  console.log(details);
+
   return (
     <Box sx={{ width: "100%" }}>
-      <InfoHeader />
+      <InfoHeader details={details} />
       <Box
         display="flex"
         sx={{ justifyContent: "center", alignItems: "flex-start" }}
       >
         <SeciliDersler />
-        <SecDersler />
+        <SecDersler data={data} />
       </Box>
     </Box>
   );
